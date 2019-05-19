bf3d9bc3b150d3dc6351d5d3e97b6cfd2cb6dbfe
Madharjan <madhav.maharjan@gmail.com>
2019-05-20 05:13:09 +0800

    wkhtmltopdf install fix

--- services/odoo/odoo.sh
+++ services/odoo/odoo.sh
@@ -12,37 +12,27 @@ ODOO_BUILD_PATH=/build/services/odoo
 ## Install Odoo Server
 apt-get install -y --no-install-recommends \
 build-essential \
-fontconfig \
 git \
-libevent-dev \
-libfreetype6-dev \
-libjpeg8-dev \
-libjpeg-dev \
-liblcms2-dev \
-liblcms2-utils \
-libldap2-dev \
-libpq-dev \
-libpython3-dev \
-libsasl2-dev \
-libtiff5-dev \
-libwebp-dev \
-libxml2-dev \
-libxslt1-dev \
-libxslt1-dev \
-libyaml-dev \
 nodejs \
-node-less \
 npm \
-pkg-config \
 postgresql-client \
 python3 \
 python3-pip \
-python3-tk \
-tcl8.6-dev \
-tk8.6-dev \
-zlib1g-dev \
+iproute2 \
+libpython3-dev \
+libsasl2-dev \
+libldap2-dev \
 uuid-runtime \
-iproute2
+fontconfig \
+libfreetype6 \
+libjpeg-turbo8 \
+libpng12-0 \
+libx11-6 \
+libxcb1  \
+libxext6  \
+libxrender1 \
+xfonts-75dpi \
+xfonts-base
 
 pip3 install setuptools --upgrade
 pip3 install wheel
@@ -62,11 +52,11 @@ pip3 install odoorpc
 
 npm install -g less less-plugin-clean-css
 
-wget -nv https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.stretch_amd64.deb 
-dpkg --force-depends -i wkhtmltox_0.12.5-1.stretch_amd64.deb
+wget -nv https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.xenial_amd64.deb
+dpkg --force-depends -i wkhtmltox_0.12.5-1.xenial_amd64.deb
 
 apt-get install -y -f --no-install-recommends
-rm -rf wkhtmltox_0.12.5-1.stretch_amd64.deb
+rm -rf wkhtmltox_0.12.5-1.xenial_amd64.deb
 
 ln -s /usr/local/bin/wkhtmltopdf /usr/bin
 ln -s /usr/local/bin/wkhtmltoimage /usr/bin
@@ -87,21 +77,9 @@ cp /opt/odoo/debian/logrotate /etc/logrotate.d/odoo
 
 ## Clean up
 apt-get remove -y \
-libevent-dev \
-libfreetype6-dev \
-libjpeg8-dev \
-libjpeg-dev \
-liblcms2-dev \
-libldap2-dev \
-libpq-dev \
 libpython3-dev \
 libsasl2-dev \
-libtiff5-dev \
-libwebp-dev \
-libxml2-dev \
-libxslt1-dev \
-libxslt1-dev \
-libyaml-dev \
+libldap2-dev
 
 rm -rf /root/.cache
 rm -rf /opt/odoo/.github
