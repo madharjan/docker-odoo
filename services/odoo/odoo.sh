92f65c55b1c6485929132750c22059e3408b0359
Madharjan <madhav.maharjan@gmail.com>
2019-05-20 15:22:59 +0800

    Simplify node 8 install

--- services/odoo/odoo.sh
+++ services/odoo/odoo.sh
@@ -9,7 +9,11 @@ fi
 
 ODOO_BUILD_PATH=/build/services/odoo
 
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+echo 'deb https://deb.nodesource.com/node_8.x xenial main' > /etc/apt/sources.list.d/nodesource.list
+echo 'deb-src https://deb.nodesource.com/node_8.x xenial main' >> /etc/apt/sources.list.d/nodesource.list
+
+apt-get update
 
 ## Install Odoo Server
 apt-get install -y --no-install-recommends \
